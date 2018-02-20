0ef687a465b6b076267085d16a17a61d8ceaa1eb
AmbulanceMan <duanganucha@hotmail.com>
2018-02-20 12:08:27 +0700

    ng-angualarfire-work2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseconfig),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebaseconfig: {
+    apiKey: "AIzaSyDz-B2-0vlWDYLIuMedWC_54Bsq-n_9mWU",
+    authDomain: "ionic-firebase-crud-4504e.firebaseapp.com",
+    databaseURL: "https://ionic-firebase-crud-4504e.firebaseio.com",
+    projectId: "ionic-firebase-crud-4504e",
+    storageBucket: "ionic-firebase-crud-4504e.appspot.com",
+    messagingSenderId: "831441420857"
+}
 };
