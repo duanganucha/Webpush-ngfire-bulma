4825bdc6830871f8cd7e61342d114c8a048a97a9
AmbulanceMan <duanganucha@hotmail.com>
2018-02-20 12:13:17 +0700

    ng-angualarfire-toast

---
+++ src/app/toast.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+import {
+  AngularFireDatabase,
+  FirebaseObjectObservable,
+  FirebaseListObservable
+} from 'angularfire2/database-deprecated';
+
+export class Message {
+  content: string;
+  style: string;
+  dismissed: boolean = false;
+
+  constructor(content, style?) {
+    this.content = content
+    this.style = style || 'info'
+  }
+}
+
+@Injectable()
+export class ToastService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  getMessages(): FirebaseListObservable<Message[]> {
+    return this.db.list('/messages', );
+  }
+
+  sendMessage(content, style) {
+    const message = new Message(content, style)
+    this.db.list('/messages').push(message)
+  }
+
+  dismissMessage(messageKey) {
+    this.db.object(`messages/${messageKey}`).update({'dismissed': true})
+  }
+
+
+}
